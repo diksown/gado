d0eebbffafd205516d176a2072248feed629933c
dikson <diksonfer@gmail.com>
2021-11-29 01:17:37 -0300

    add initial test

---
+++ test/errors.cpp
@@ -0,0 +1,33 @@
+// This file has a lot of intentional errors to test the program.
+#include <bits/stdc++.h>
+
+// #include <missing_bracket_and_file // fatal error
+#define number_of_args_mismatch(x, y) 1
+#define redefined_define 1
+#define redefined_define 2
+
+void void_with_a_value(){
+   return 42;
+}
+
+int main() {
+	char many_chars = 'many';
+	alignof(void);
+	1 = "lvalue_required";
+	number_of_args_mismatch(1, 2, 3);
+	number_of_args_mismatch(1);
+	char many_chars = 'many';
+	int *pointer_sum_1, *pointer_sum_2;
+	pointer_sum_1 = pointer_sum_1 + pointer_sum_2;
+	void int voidint;
+	int small_array[-1];
+	int isnt_known[];
+	undefined_variable = 0;
+	int div_by_zero = 1/0;
+	long long no_strings = "hey!";
+	double I_forgot_the_colon
+	int just_a_message;
+	int missing_closing_stuff = (1+1;
+	int does_this_compile = (1+1);
+	pair<> d;
+}

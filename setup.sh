001fd2b9e93a68109bf05a9d3267a6449190b1fc
dikson <diksonfer@gmail.com>
2021-12-04 00:57:06 -0300

    mostly small fixes

--- setup.sh
+++ setup.sh
@@ -1,20 +1,45 @@
-# install
-# sudo cp gado /usr/local/bin/
-# sudo ln -s /usr/local/bin/gado /usr/local/bin/gado++
+check_if_sudo() {
+	if [[ $EUID -ne 0 ]]; then
+		echo "you should run this with sudo." 
+		exit 1
+	fi
+}
 
-# sudo mkdir /usr/local/share/gado
-# cp data/poetry.json /usr/local/share/
+install_gado() {
+	cp gado /usr/bin/
+	ln -s /usr/bin/gado /usr/bin/gado++
+	# TODO: replace this for something better (requirements.txt or smth)
+	pip install -q pronouncing
+	mkdir /usr/share/gado
+	cp data/poetry.json /usr/share/gado/
+}
+
+uninstall_gado() {
+	# TODO: maybe remove pronouncing?
+	rm /usr/bin/gado
+	rm /usr/bin/gado++
+	rm -r /usr/share/gado
+	pip uninstall -qy pronouncing
+}
 
 if [[ $1 == "install" ]]; then
-	echo "Installing gado..."
-	# do stuff
-	echo "gado installed! Run `gado` to start."
+	check_if_sudo
+	echo "installing gado..."
+	install_gado
+	echo "gado installed! Run 'gado' to start."
 elif [[ $1 == "uninstall" ]]; then
-	# TODO: prompt user
-	echo "Uninstalling gado..."
-	# do stuff
+	check_if_sudo
+	echo -n "do you want to uninstall gado? [y/n] "
+	read answer
+	if [[ $answer != "y" ]]; then
+		exit
+	fi
+	uninstall_gado
+	echo "uninstalling gado..."
+	
 	echo "gado uninstalled."
 else
-	echo "To install, type ./setup.sh install"
-	echo "To uninstall, type ./setup.sh uninstall"
+	echo "setup for gado - generate poetry using gcc!"
+	echo ""
+	echo "usage: sudo ./setup.sh [install|uninstall]"
 fi

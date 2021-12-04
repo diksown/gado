05768df81f70b94d70eedb14719d8172fa94ee73
dikson <diksonfer@gmail.com>
2021-12-04 13:59:43 -0300

    improve setup

--- setup.sh
+++ setup.sh
@@ -6,41 +6,56 @@ check_if_sudo() {
 }
 
 command_exists() {
-	command -v "$1" >/dev/null 2>&1 || {
+	command -v "$1" >/dev/null 2>&1
+}
+
+ask_to_install() {
+	command_exists $1 || {
 		echo $1 "is not installed. please install it first."
 		exit 1
-	} 
+	}
 }
 
 install_gado() {
-	command_exists gcc
-	command_exists pip
-	cp gado.py /usr/bin/gado
-	ln -s /usr/bin/gado /usr/bin/gado++
+	if command_exists gado; then
+		echo "gado is already installed. updating..."
+	else
+		echo "installing gado..."
+	fi
+
+	ask_to_install gcc
+	ask_to_install pip
 
 	# TODO: replace this for something better (requirements.txt or smth)
 	pip install -q pronouncing
-	
+
 	mkdir -p /usr/share/gado/data
 	echo "downloading poetry database... (~5.5MB)"
-	curl --progress-bar https://www.gutenberg.org/files/100/100-0.txt --output /usr/share/gado/data/shakespeare.txt
+	curl --progress-bar https://www.gutenberg.org/files/100/100-0.txt \
+		--output /usr/share/gado/data/shakespeare.txt
 	python create_db.py
+
+	cp gado.py /usr/bin/gado
+	ln -sf /usr/bin/gado /usr/bin/gado++
+	echo "gado installed! Run 'gado' to start."
 }
 
 uninstall_gado() {
-	rm /usr/bin/gado
-	rm /usr/bin/gado++
-	rm -r /usr/share/gado
+	echo "uninstalling gado..."
+
+	rm -f /usr/bin/gado
+	rm -f /usr/bin/gado++
+	rm -rf /usr/share/gado
 
 	# TODO: should you really remove pronouncing?
-	pip uninstall -qy pronouncing
+	pip uninstall -qy pronouncing >/dev/null 2>&1
+
+	echo "gado uninstalled."
 }
 
 if [[ $1 == "install" ]]; then
 	check_if_sudo
-	echo "installing gado..."
 	install_gado
-	echo "gado installed! Run 'gado' to start."
 elif [[ $1 == "uninstall" ]]; then
 	check_if_sudo
 	echo -n "do you want to uninstall gado? [y/n] "
@@ -49,9 +64,6 @@ elif [[ $1 == "uninstall" ]]; then
 		exit
 	fi
 	uninstall_gado
-	echo "uninstalling gado..."
-	
-	echo "gado uninstalled."
 else
 	echo "setup for gado - generate poetry using gcc!"
 	echo ""

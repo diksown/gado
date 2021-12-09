9c1d1adfef949fc2d93629834b6c3eadeef28832
dikson <diksonfer@gmail.com>
2021-12-09 00:52:46 -0300

    remove setup.sh file, quick fixes

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='gado',
+    version='0.0.1',
+    license='MIT',
+    author="Dikson Santos",
+    author_email='diksonfer@gmail.com',
+    packages=find_packages('gado'),
+    package_dir={'': 'gado'},
+    url='https://github.com/diksown/gado',
+    keywords='gcc poetry nlp',
+    install_requires=[
+        'pronouncing',
+        'requests',
+    ],
+)

ca52b82e9af51675386f658e1e8456c5c606d2ea
dikson <diksonfer@gmail.com>
2021-12-10 00:34:15 -0300

    add readme to pypi

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='gado',
-    version='0.0.1',
+    version='0.0.2',
     license='MIT',
     author="Dikson Santos",
     author_email='diksonfer@gmail.com',
+    description="generate poetry with gcc diagnostics",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     packages=find_packages('gado'),
     package_dir={'': 'gado'},
     url='https://github.com/diksown/gado',
@@ -21,4 +27,10 @@ setup(
             'gado++ = gado:main',
         ],
     },
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Linux",
+        "Environment :: Console",
+    ),
 )
